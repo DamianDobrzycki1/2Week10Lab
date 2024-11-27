505792d1b8ade732bcdfc40244189eddd6fb61e2
G00419511@atu.ie <G00419511@atu.e>
2024-11-27 14:41:44 +0000

    Another one

--- src/main/java/ie/atu/week11example/PersonService.java
+++ src/main/java/ie/atu/week11example/PersonService.java
@@ -4,10 +4,18 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PersonService {
+    private PaymentClient paymentClient;
+    public PersonService(PaymentClient paymentClient) {
+        this.paymentClient = paymentClient;
+    }
     // Placeholder method to save a person (add to Db in the next two weeks)
     public void savePerson(Person person) {
-        System.out.println("Person saved: " + person);
+        String response = paymentClient.makePayment(person);
+        System.out.println("Person saved: " + person + " " + response);
+        System.out.println(response);
     }
+
+
 
     // Placeholder method to retrieve a person by employeeId
     public Person getPersonByEmployeeId(String employeeId) {
